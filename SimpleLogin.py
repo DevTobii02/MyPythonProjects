d250044c4a7147b899fff853ebb59521844c424c
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-02-25 00:51:01 -1200

    Able to intialize transaction functions and the sign up and login works perfectly except for account number generation

--- SimpleLogin.py
+++ SimpleLogin.py
@@ -1,24 +1,50 @@
-print("Welcome") 
+users = {}  
+def SignUp():
+    print("Fill In The Following Fields To Sign Up Now")
+    SignUp_Username = input("Enter A Username : ")
+    SignUp_Password = input("Enter A Password : ") 
+    users[SignUp_Username] = SignUp_Password
+    print("Sign Up Successfull, Kindly Proceed To Login")
 
-Choice = input("Enter Your Choice (Login or Sign Up) : ") 
-
-if Choice .strip() .lower() == "Sign Up":
-    print("Fill In The Following Fields To Sign Up")
-    Username = input("Enter A Valid Username : ")
-    Password = input("Provide A Valid Password : ")
-    print("Account Created Successfully")
-    print("Proceed To Login Now !!!")
+def Login():
+    print("Login") 
+    print("Please Provide Correct Information To The Following Fields") 
+    Login_Username = input("Enter Your Login Username : ") 
+    Login_Password = input("Provide Your Correct Password : ") 
+    if Login_Username in users and Login_Password == users[Login_Username]:
+        print(f"Login Successfull Welcome Back , {Login_Username}")
+        Activity = input(f"Choose An Activity (Deposit , Withdraw , Send Money , Check Balance , Save Money) : ").strip().lower()
+        if Activity == "Deposit":
+            DepositFunds()
+        elif Activity == "Withdraw":
+            WithdrawFunds()
+        elif Activity == "Send Money":
+            SendMoney()
+        elif Activity == "Check Balance":
+            CheckBalance()
+        elif Activity == "Save Money":
+            SaveMoney()
+        else:
+            print("Invalid Request")
+    else:
+        print("Invalid Username or Password")  
+        
+def DepositFunds():
+    pass
+def WithdrawFunds():
+    pass
+def SendMoney():
+    pass        
+def CheckBalance():
+    pass
+def SaveMoney():
+    pass
+print("Welcome !!! (Sign Up or Login)") 
+Choice = input("Login or Sign Up : ").strip().lower() 
+if Choice == "Sign Up".strip() .lower():
+    SignUp()
+    Login()
 elif Choice == "Login":
-    print("Fill In The Following Fields With The Correct Information")  
-    Username1 = input("Enter Your username : ") 
-    Password1 = input("Enter Your password : ")
-if Username == Username1 and Password == Password1:
-    print("Logged In Succesfully")
+    Login()
 else:
-    print("Invalid username or password ")
-
-    
-if Username == Username1 and Password == Password1:
-    print("Logged In Succesfully")
-else:
-    print("Invalid username or password ")
+    print("Invalid Choice")      
