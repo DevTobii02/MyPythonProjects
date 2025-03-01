1dcd7de49f4de91761b72ec3f7ee7a912969d4c8
Adebayo Abdullahi <tobiithedev@gmail.com>
2025-02-28 15:59:19 -1200

    Created Bank and Account Classes and declared functions under them

---
+++ OOPModel BankProgram.py
@@ -0,0 +1,33 @@
+import sys
+import random 
+class Account:
+    def __init__(self, Name, Age, Email, Password, AccountNumber, Balance): 
+        pass  
+    
+    def Deposit(self):
+        pass
+    
+    def Withdraw(self):
+        pass
+    
+    def CheckBalance(self):
+        pass
+    
+    def PrintInfo(self):
+        pass
+    
+class Bank:
+    def __init__(self):
+        self.Customers = {}
+    
+    def GenerateAccountNumber(self):
+        pass
+    
+    def NewUser(self):
+        pass
+    
+    def ExsistingUser(self):
+        pass    
+    
+    def MenuList(self):
+        pass
